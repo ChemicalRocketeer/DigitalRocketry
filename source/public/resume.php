c7947297ee5b14848f1532c3044a8e81df6a64ff
David Aaron Suddjian <aasuddjian@gmail.com>
2016-03-23 11:50:35 -0700

    hide .php file extensions in urls

---
+++ source/public/resume.php
@@ -0,0 +1,121 @@
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <link href='https://fonts.googleapis.com/css?family=Open+Sans:400italic,400|EB+Garamond|Alegreya+Sans+SC:300,400' rel='stylesheet' type='text/css'>
+    <link href='resume.css' rel='stylesheet' type='text/css'>
+    <title>Resume - David Aaron Suddjian</title>
+  </head>
+  <body>
+    <div class="wrapper">
+      <h1 class="page-title">David Aaron Suddjian</h1>
+      <header class="personal-info">
+        <h2>Personal Data</h2>
+        <dl>
+          <dt class="website">Website</dt>
+          <dd><a href="http://digitalrocketry.com">digitalrocketry.com</a></dd>
+          <dt class="email">Email</dt>
+          <dd><a href="mailto:aaron@digitalrocketry.com">aaron@digitalrocketry.com</a></dd>
+          <dt class="phone">Phone</dt>
+          <dd class="number">831 713 8666</dd>
+          <dt class="github">GitHub</dt>
+          <dd>github.com/ChemicalRocketeer</dd>
+        </dl>
+        <blockquote>
+          <p>I am passionate about software engineering and algorithm design. I solve complex business problems with systems that are powerful, efficient, and beautiful. I care deeply about the quality of my work.</p>
+        </blockquote>
+      </header>
+      <section class="accomplishments">
+        <h2>Accomplishments</h2>
+        <ul>
+          <li>Engineered a geographical database of over <span class="number">200,000</span> listings for CampusCribz, with a custom search algorithm tailored to students and tuned to maximize conversion rates.</li>
+          <li>Worked as a freelance software engineering contractor for a year, reaching multiple clients.</li>
+          <li>Built an Android app providing live restaurant information and collecting statistics for the owners.</li>
+          <li>Hosted a 24-hour live-streamed game dev fundraiser for Children’s Hospital Oakland, raising over <span class="number">$500</span>.</li>
+          <li>Organized workshops for struggling computer science students in one-on-one, personalized sessions.</li>
+          <li>Planned and carried out weekly “Hour of Code” sessions to teach programming to middle school students.</li>
+          <li>Participated in multiple hackathons and game jams with awesome groups of people.</li>
+        </ul>
+      </section>
+      <section class="skills">
+        <section class="languages">
+          <h2>Languages</h2>
+          <ul>
+            <li>Java</li>
+            <li>C#</li>
+            <li>Ruby/Rails</li>
+            <li>JavaScript</li>
+            <li>Python</li>
+            <li>PHP</li>
+            <li>HTML/CSS/SASS</li>
+          </ul>
+        </section>
+        <section class="tools">
+          <h2>Tools and Technologies</h2>
+          <ul>
+            <li>Git</li>
+            <li>Linux Shell</li>
+            <li>Eclipse</li>
+            <li>Android SDK</li>
+            <li>Unity3D</li>
+            <li>Node.js/Express</li>
+            <li>MongoDB</li>
+            <li>SQL</li>
+            <li>Bootstrap</li>
+            <li>Google Maps API</li>
+            <li>Regular Expressions</li>
+          </ul>
+        </section>
+      </section>
+      <section class="work-history">
+        <h2>Work History</h2>
+        <section class="job">
+          <section class="title">
+            <h3 class="duration"><span class="start-date">January 2016</span> <span class="present-date">Present</span></h3>
+            <h4 class="company">CampusCribz</h4>
+            <h4 class="position">Full Stack Developer</h4>
+          </section>
+          <span class="divider"></span>
+          <p class="description">Worked on virtually all aspects of the site, building a new sublet feature, map search algorithm, and moving the database to a new model using spherical geometry. Found and acted on numerous critical issues with the site.</p>
+        </section>
+        <section class="job">
+          <section class="title">
+            <h3 class="duration"><span class="start-date">June 2014</span> <span class="end-date">December 2015</span></h3>
+            <h4 class="company">Digital Rocketry</h4>
+            <h4 class="position">Freelance Software Engineer</h4>
+          </section>
+          <span class="divider"></span>
+          <p class="description">Web and Android development work for multiple clients in various capacities, sometimes working alone, sometimes with an existing team when they couldn't figure out how to do the job.</p>
+        </section>
+        <section class="job">
+          <section class="title">
+            <h3 class="duration"><span class="start-date">August 2012</span> <span class="end-date">May 2014</span></h3>
+            <h4 class="position">Full-time student at Cabrillo College</h4>
+          </section>
+          <span class="divider"></span>
+          <p class="description">Studied computer science, teaching myself extra on the side, working part-time, and tutoring other students.</p>
+        </section>
+        <section class="job">
+          <section class="title">
+            <h3 class="duration"><span class="start-date">August 2012</span> <span class="end-date">September 2013</span></h3>
+            <h4 class="company">St. Joseph's Church</h4>
+            <h4 class="position">Editor/Sys-Admin/Receptionist</h4>
+          </section>
+          <span class="divider"></span>
+          <p class="description">Edited church publications, managed the IT and website, and Often helped people with major life problems, e.g. they were homeless or in debt or grieving or suicidal.</p>
+        </section>
+        <section class="job">
+          <section class="title">
+            <h3 class="duration"><span class="start-date">2009</span> <span class="end-date">2013</span></h3>
+          </section>
+          <span class="divider"></span>
+          <p class="description">Assisted my father, a biologist, doing field measurements and biological surveys. Assisted my mother, a landscape architect, doing site analysis and measurements.</p>
+        </section>
+      </section>
+      <footer>
+        <p>References provided upon request.</p>
+      </footer>
+    </div><!--wrapper-->
+  </body>
+</html>
